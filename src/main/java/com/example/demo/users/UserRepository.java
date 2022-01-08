c0891d8f08cf05bd9956c9f6feb34379fdd4b0b1
(Clark61) <clark6147@gmail.com>
2022-01-07 22:20:07 -0600

    Added User endpoints

---
+++ src/main/java/com/example/demo/users/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}

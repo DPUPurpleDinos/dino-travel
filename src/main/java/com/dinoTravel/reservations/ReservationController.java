abbc6811fc666315a2ce4fbbb9c428d16a94f96e
(Clark61) <clark6147@gmail.com>
2022-01-23 23:20:35 -0600

    Added support for error responses. Added documentation

--- src/main/java/com/dinoTravel/reservations/ReservationController.java
+++ src/main/java/com/dinoTravel/reservations/ReservationController.java
@@ -1,53 +1,129 @@
 package com.dinoTravel.reservations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
+/**
+ * Handles exceptions that get thrown by the ReservationController
+ */
+@ControllerAdvice
+class ReservationNotFoundAdvice {
+
+    /**
+     * Generate a 404 status is a requested ID is not found
+     * and return an error message as a String
+     * @param ex ReservationNotFoundException
+     * @return Error message containing the reservation ID that caused the exception
+     */
+    @ResponseBody
+    @ExceptionHandler(ReservationNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String reservationNotFoundHandler(ReservationNotFoundException ex) { return ex.getMessage(); }
+}
 @RestController
 @RequestMapping("/api/reservations")
 public class ReservationController {
 
     @Autowired
     private ReservationRepository reservationRepository;
+    private final ReservationModelAssembler reservationAssembler;
 
-    @GetMapping
-    public List<Reservation> getAllReservations() { return reservationRepository.findAll();}
+    /**
+     * Constructor to create a RestController for Reservation objects
+     * @param repository Repository to save Reservation objects
+     * @param assembler Assembler to create the JSON response
+     */
+    ReservationController(ReservationRepository repository, ReservationModelAssembler assembler) {
+        this.reservationRepository = repository;
+        this.reservationAssembler = assembler;
+    }
 
+    /**
+     * Returns all Reservations saved in the ReservationRepository
+     * @return A collection of Reservations and their bodies as an EntityModel
+     */
+    @GetMapping()
+    CollectionModel<EntityModel<Reservation>> getAllReservations() {
+        List<EntityModel<Reservation>> reservations = reservationRepository.findAll().stream()
+            .map(reservationAssembler::toModel)
+            .collect(Collectors.toList());
+
+        return CollectionModel.of(reservations);
+    }
+
+    /**
+     * Return the body for a single reservation
+     * @param reservationId the ID for the reservation
+     * @return The body of the reservation as an EntityModel
+     */
     @GetMapping("/{id}")
-    public Reservation getReservationById(@PathVariable ("id") int reservationId) {
-        return reservationRepository.findById(reservationId).orElseThrow(() -> new NoSuchElementException("Could not find reservation with id: " + reservationId));
+    EntityModel<Reservation> getReservationById(@PathVariable ("id") int reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new ReservationNotFoundException(reservationId));
+
+        return reservationAssembler.toModel(reservation);
     }
 
+    /**
+     * Update an existing reservation already contained in the ReservationRepository
+     * Otherwise save it to the ReservationRepository
+     * @param reservation The body of the reservation
+     * @param reservationId The ID for an existing reservation
+     * @return The body of the updated reservation as a ResponseEntity
+     */
     @PutMapping("/{id}")
-    public Reservation updateReservation(@RequestBody Reservation reservation, @PathVariable("id") int reservationId) {
-        Reservation existingReservation = reservationRepository.findById(reservationId).orElseThrow(() -> new NoSuchElementException("Could not find reservation with id: " + reservationId));
+    ResponseEntity<?> updateReservation(@RequestBody Reservation reservation, @PathVariable ("id") int reservationId) {
+        Reservation existingReservation = reservationRepository.findById(reservationId)
+            .map(newReservation -> {
+                newReservation.setUser_id(reservation.getUser_id());
+                newReservation.setTrip_type(reservation.getTrip_type());
+                newReservation.setOutgoing_flight_type(reservation.getOutgoing_flight_type());
+                newReservation.setOutgoing_flight_id(reservation.getOutgoing_flight_id());
+                newReservation.setReturning_flight_type(reservation.getReturning_flight_type());
+                newReservation.setReturning_flight_id(reservation.getReturning_flight_id());
+                newReservation.setPrice(reservation.getPrice());
+                return reservationRepository.save(newReservation);
+            }).orElseGet(() -> {
+                reservation.setReservation_id(reservationId);
+                return reservationRepository.save(reservation);
+            });
+        EntityModel<Reservation> entityModel = reservationAssembler.toModel(existingReservation);
 
-        existingReservation.setUser_id(reservation.getUser_id());
-        existingReservation.setTrip_type(reservation.getTrip_type());
-        existingReservation.setOutgoing_flight_type(reservation.getOutgoing_flight_type());
-        existingReservation.setOutgoing_flight_id(reservation.getOutgoing_flight_id());
-        existingReservation.setReturning_flight_type(reservation.getReturning_flight_type());
-        existingReservation.setReturning_flight_id(reservation.getReturning_flight_id());
-        existingReservation.setPrice(reservation.getPrice());
-
-        return reservationRepository.save(existingReservation);
+        return ResponseEntity
+            .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+            .body(entityModel);
     }
 
+    /**
+     * Create a new reservation to be added to the ReservationRepository
+     * @param reservation The body of the reservation
+     * @return The body of the created reservation as a ResponseEntity
+     */
     @PostMapping
-    public Reservation createReservation(@RequestBody Reservation reservation) {
-        return reservationRepository.save(reservation);
+    ResponseEntity<?> createReservation(@RequestBody Reservation reservation) {
+        EntityModel<Reservation> entityModel = reservationAssembler.toModel(reservationRepository.save(reservation));
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
+    /**
+     * Delete a reservation from the ReservationRepository
+     * @param reservationId The ID for a reservation to delete
+     * @return An empty body as a ResponseEntity
+     */
     @DeleteMapping
-    public ResponseEntity<Reservation> deleteReservation(@PathVariable("id") int reservationId) {
-        Reservation existingReservation = reservationRepository.findById(reservationId).orElseThrow(() -> new NoSuchElementException("Could not find reservation with id: " + reservationId));
+    ResponseEntity<?> deleteReservation(@PathVariable ("id") int reservationId) {
+        reservationRepository.findById(reservationId).orElseThrow(() -> new ReservationNotFoundException(reservationId));
 
-        reservationRepository.delete(existingReservation);
+        reservationRepository.deleteById(reservationId);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/dinoTravel/users/UserController.java
+++ src/main/java/com/dinoTravel/users/UserController.java
@@ -1,53 +1,130 @@
 package com.dinoTravel.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
+/**
+ * Handles exceptions that get thrown by the UserController
+ */
+@ControllerAdvice
+class UserNotFoundAdvice {
+
+    /**
+     * Generate a 404 status if a requested ID is not not found
+     * and returns an error message as a String
+     * @Param ex UserNotFoundException
+     * @Return Error message containing the user ID that caused the exception
+     */
+    @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String userNotFoundHandler(UserNotFoundException ex) { return ex.getMessage(); }
+}
+
+/**
+ * Handles HTTP requests for User objects
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    private final UserModelAssembler userAssembler;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    /**
+     * Constructor to create a RestController for User objects
+     * @param repository Repository to save the User objects
+     * @param assembler Assembler to create the JSON response
+     */
+    UserController(UserRepository repository, UserModelAssembler assembler) {
+        this.userRepository = repository;
+        this.userAssembler = assembler;
     }
 
+    /**
+     * Returns all Users saved in the UserRepository
+     * @return A collection of Users and their bodies represented as an EntityModel
+     */
+    @GetMapping()
+    CollectionModel<EntityModel<User>> getAllUsers() {
+        List<EntityModel<User>> users = userRepository.findAll().stream()
+            .map(userAssembler::toModel)
+            .collect(Collectors.toList());
+
+        return CollectionModel.of(users);
+    }
+
+    /**
+     * Return the body for a single user
+     * @param userId the ID for the user
+     * @return The body of the user as an EntityModel
+     */
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable ("id") int userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("Could not find user with id: " + userId));
+    EntityModel<User> getUserById(@PathVariable ("id") int userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+
+        return userAssembler.toModel(user);
     }
 
+    /**
+     * Update an existing user already contained in the UserRepository
+     * Otherwise save it to the UserRepository
+     * @param user The body of the user
+     * @param userId The ID for an existing user
+     * @return The body of the updated user as a ResponseEntity
+     */
     @PutMapping("/{id}")
-    public User updateFlight(@RequestBody User user, @PathVariable("id") int userId) {
-        User existingUser = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("Could not find user with id: " + userId));
+    ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable ("id") int userId) {
+        User existingUser = userRepository.findById(userId)
+            .map(newUser -> {
+                newUser.setFirst_name(user.getFirst_name());
+                newUser.setLast_name(user.getLast_name());
+                newUser.setDob(user.getDob());
+                newUser.setEmail(user.getEmail());
+                return userRepository.save(newUser);
+            }).orElseGet(() -> {
+                user.setUser_id(userId);
+                return userRepository.save(user);
+            });
+        EntityModel<User> entityModel = userAssembler.toModel(existingUser);
 
-        existingUser.setFirst_name(user.getFirst_name());
-        existingUser.setLast_name(user.getLast_name());
-        existingUser.setEmail(user.getEmail());
-        existingUser.setDob(user.getDob());
-
-        return userRepository.save(existingUser);
+        return ResponseEntity
+            .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+            .body(entityModel);
     }
 
+    /**
+     * Create a new user to be added to the UserRepository
+     * @param user The body of the user
+     * @return The body of the created user as a ResponseEntity
+     */
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    ResponseEntity<?> createUser(@RequestBody User user) {
+        EntityModel<User> entityModel = userAssembler.toModel(userRepository.save(user));
+
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
-    @DeleteMapping
-    public ResponseEntity<User> deleteUser(@PathVariable("id") int userId) {
-        User existingUser = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("Could not find user with id: " + userId));
+    /**
+     * Delete a user from the UserRepository
+     * @param userId The ID for a user to delete
+     * @return An empty body as a ResponseEntity
+     */
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> deleteUser(@PathVariable ("id") int userId) {
+        userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
 
-        userRepository.delete(existingUser);
+        userRepository.deleteById(userId);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
-
 }

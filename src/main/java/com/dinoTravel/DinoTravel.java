28dd7361d46b57c632c14d82d0a6af92c7e14e23
David Gawel <DGawel77@gmail.com>
2022-04-09 21:39:44 -0500

    Updated SSL cert to renew automatically

--- src/main/java/com/dinoTravel/DinoTravel.java
+++ src/main/java/com/dinoTravel/DinoTravel.java
@@ -1,12 +1,15 @@
 package com.dinoTravel;
 
+import java.security.Security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 
 @SpringBootApplication
 public class DinoTravel {
 
 	public static void main(String[] args){
+		Security.addProvider(new PemKeyStoreProvider());
 		SpringApplication.run(DinoTravel.class, args);
 	}
 
